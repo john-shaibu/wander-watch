8f7d38ebf86f87c1526d81c9c691cc410984f27c
john shaibu <johnmaxwell059@gmail.com>
2024-03-02 02:02:25 +0100

    restyled the login page

--- frontend-n/wander-watch/src/assets/index.jsx
+++ frontend-n/wander-watch/src/assets/index.jsx
@@ -1,5 +1,9 @@
 import BlackLogo from './logoblack.svg?react'
 import WhiteLogo from './logoWhite.svg?react'
+import MailIcon from './mailIcon.svg?react'
+import PasscodeIcon from './passcodeIcon.svg?react'
+import FullLogo from './fullLogo.svg?react'
 
 
-export { BlackLogo, WhiteLogo }
+
+export { BlackLogo, WhiteLogo, MailIcon, PasscodeIcon, FullLogo }
--- frontend-n/wander-watch/src/pages/Login.jsx
+++ frontend-n/wander-watch/src/pages/Login.jsx
@@ -1,38 +1,40 @@
-
-
+import { FullLogo } from "../assets";
+import { Link } from "react-router-dom";
+import '../styles/login.css'
 
 const Login = () => {
   return (
     <>
-      <div className="login">
-        <form>
-          <h3>Login to your account</h3>
-          {/* <label>Email or username</label> */}
-          <p>Email Address</p>
-          <input type="email" placeholder="Ex. kygiddy@example.com" />
-
-          {/* <label>Password</label> */}
-          <p>Password</p>
-          <input type="text" placeholder="Password" />
-
-          <div className="switch">
-            {/* <input type="checkbox" id="switch" checked /> */}
-            {/* <label for="switch" class="custom-checkbox"></label> */}
-            <br /><br />
-            <span>Remember me</span>{" "}
-            <span>
-              {" "}
-              <a href="!#">Forgot your Password?</a>
-            </span>
+      <div className="container">
+        <div className="login">
+          <a href="#" className="logoLink">
+            <FullLogo />
+          </a>
+          <div className="formContainer">
+            <h2>Login to your account</h2>
+            <form action="" method="post">
+              <div>
+                <label htmlFor="user_email">Email Address</label>
+                <input type="email" name="user_email" id="user_email" placeholder="Ex: johndoe@domain.com" />
+              </div>
+              <div>
+                <label htmlFor="user_password">Password</label>
+                <input type="password" name="user_password" id="user_password" placeholder="Password" />
+              </div>
+              <div className="other_login_form_actions">
+                <div className="remember_me_checkbox">
+                  <label htmlFor="remember_me">
+                    <input type="checkbox" name="remember_me" id="remember_me" />
+                    Remember me
+                  </label>
+                </div>
+                <Link to='/register' className="forgot_password_link">Forgot password?</Link>
+              </div>
+              <button type="submit" className="primary-btn">Login to your account</button>
+            </form>
+            <p>Don't have an account yet? <Link to='/register'>Create account</Link></p>
           </div>
-          <div className="in">
-            <br /><br />
-            <button className="primary-btn">
-              <a href="!#">Log in to your account</a>
-            </button>
-          </div>
-          <p>Dont have an account yet? <span><a href="!#">Sign up</a></span></p>
-        </form>
+        </div>
       </div>
     </>
   );
