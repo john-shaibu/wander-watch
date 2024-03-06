489950814ce91af2676f3d820ae25a70fa274867
bhrayhaw <elijahapreko@gmail.com>
2024-03-06 20:16:28 +0000

    added validation using joi

---
+++ backend/src/validation/userValidation.js
@@ -0,0 +1,47 @@
+const Joi = require('joi');
+
+const registrationValidation = Joi.object({
+  fullname: Joi.string().min(6).required(),
+  email: Joi.string().required().email(),
+  password: Joi.string()
+    .min(8)
+    .max(40)
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/
+    )
+    .message(
+      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.'
+    )
+    .required(),
+  confirmpassword: Joi.string()
+    .min(8)
+    .max(40)
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/
+    )
+    .message(
+      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.'
+    )
+    .required(),
+});
+
+const loginValidation = Joi.object({
+  email: Joi.string().required().email(),
+  password: Joi.string()
+    .min(8)
+    .max(40)
+    .pattern(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}|:"<>?~`\-=[\]\\;',./]).{8,40}$/
+    )
+    .message(
+      '"{#label}" must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol, and be 8-40 characters.'
+    )
+    .required(),
+});
+
+const updateUserValidation = Joi.object({
+  fullname: Joi.string().min(6).required(),
+  email: Joi.string().required().email(),
+});
+
+module.exports = { registrationValidation, loginValidation, updateUserValidation };
