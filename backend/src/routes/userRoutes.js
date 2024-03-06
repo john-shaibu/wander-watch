c918a30a08544e385f2216653430407b2994eab6
bhrayhaw <elijahapreko@gmail.com>
2024-03-06 20:19:22 +0000

    added routes for the user

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express')
-const { register_user } = require('../controllers/userController')
+const { registerUser, loginUser, updatePassword, updateUser } = require('../controllers/userController')
+const { verifyToken } = require('../middlewares/jwt')
 
 const userRouter = express.Router()
 
 // Register User
-userRouter.post('/register', register_user)
+userRouter.post('/register', registerUser)
+
+// Login User
+userRouter.post('/login', loginUser)
+
+// Update User without password
+userRouter.put('/update', verifyToken, updateUser)
+
+// Update User password
+userRouter.put('/update-password', updatePassword)
 
 module.exports = userRouter
