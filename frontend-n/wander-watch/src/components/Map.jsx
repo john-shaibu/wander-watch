c8af9962a401aa3437b953c98fe3d2bad7a33ec5
john shaibu <johnmaxwell059@gmail.com>
2024-03-08 15:09:55 +0100

    some error refactoring

--- frontend-n/wander-watch/src/components/Map.jsx
+++ frontend-n/wander-watch/src/components/Map.jsx
@@ -1,41 +1,53 @@
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import { LocationIcon, PreciseLocation, ZoomInIcon, ZoomOutIcon } from '../assets'
 import useLocation from '../hooks/useLocation'
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-const CustomMarker = () => {
+const CustomMarker = ({ title = '' }) => {
     const [position, setPosition] = useState(null)
     const map = useMapEvents({
       click() {
         map.locate()
+        map.zoomIn(5)
       },
       locationfound(e) {
         setPosition(e.latlng)
         map.flyTo(e.latlng, map.getZoom())
       },
     })
+    console.log(position)
     return position === null ? null : (
         <Marker position={position}>
-          <Popup>You are here</Popup>
+          <Popup>{title}</Popup>
         </Marker>
       )
 }
 
 const Map = () => {
   const [err, location] = useLocation()
+  const mapRef = useRef(null)
+  console.log(location);
+  const locationTitle = 'Kenneth Mellanby Hall, UI'
+
+  useEffect(() => {
+    if (location && mapRef.current) { 
+        mapRef.current._container.click() 
+        mapRef.current._container.click() 
+    }
+  }, [location, mapRef])
 
   return (
     <div className='map-container'>
-        {!location ? null : <MapContainer zoom={5} center={[location.longitude, location.latitude]}>
+        {!location ? null : <MapContainer zoom={8} center={{lng: location.longitude, lat: location.latitude}} ref={mapRef}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <CustomMarker />
+            <CustomMarker title={locationTitle}/>
         </MapContainer>}
         <div className="user_location_info">
             <div className="location_info">
                 <LocationIcon />
-                <b>Kenneth Mellanby Hall, UI</b>
+                <b>{locationTitle}</b>
             </div>
             <div className="map_controls">
                 <button className="zoom_out">
@@ -46,7 +58,7 @@ const Map = () => {
                 </button>
                 <button className="go_to_precise_location primary-btn">
                     <PreciseLocation />
-                </button>
+                 </button>
             </div>
         </div>
     </div>
