840d1505b936c16f9152453dd97d814b72e86e3e
john shaibu <johnmaxwell059@gmail.com>
2024-03-09 15:34:28 +0100

    johnbaba

--- frontend-n/wander-watch/src/components/Filter.jsx
+++ frontend-n/wander-watch/src/components/Filter.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { LocationIcon } from '../assets'
 
 const Filter = () => {
   return (
-    <div>Filter</div>
+    <div>
+        <div className="filter_header">
+            <LocationIcon />
+            <b>Filter locations</b>
+        </div>
+        <div className="filter_form">
+            <form action="" method="get">
+                <div className='date_filter'>   
+                    <span>Date</span>
+                    <div>
+                        <div>
+                            <label htmlFor="date_from">From</label>
+                            <input type="date" name="date_from" id="date_from" />
+                        </div>
+                        <div>
+                            <label htmlFor="date_to">To</label>
+                            <input type="date" name="date_to" id="date_to" />
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
 
