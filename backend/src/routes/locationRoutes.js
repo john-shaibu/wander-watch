79296ee746c085275222dc699dd90fbfcd9774c6
john shaibu <johnmaxwell059@gmail.com>
2024-03-15 22:51:35 +0100

    modification

--- backend/src/routes/locationRoutes.js
+++ backend/src/routes/locationRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const locationRouter = express.Router();
 const createLocation = require('../controllers/createLocation');
-const getLocation = require('../controllers/getLocationHistory');
+const getLocations = require('../controllers/getLocationHistory');
 const { loggedInMidddleware } = require('../middlewares/authMiddlewares');
 
 // Create a new location
 locationRouter.post('/', loggedInMidddleware, createLocation);
 
 // Get location history
-locationRouter.get('/', loggedInMidddleware, getLocation);
+locationRouter.get('/', loggedInMidddleware, getLocations);
 
 module.exports = locationRouter;
